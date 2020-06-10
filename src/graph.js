2e47172b557bd43ada21a1c328e6349d84f60fc0
thanya <32271882+ThanyaSanthosh@users.noreply.github.com>
2020-06-10 21:12:41 +0530

    bfs with looping

--- src/graph.js
+++ src/graph.js
@@ -4,8 +4,37 @@
 // To => to 
 // Should return true.
 
+const getAdjacentList = function (pairs) {
+  const adjacentList = {};
+  pairs.forEach(([from,to]) => {
+    if (adjacentList[from] === undefined) {
+      adjacentList[from] = [to];
+    } else {
+      adjacentList[from].push(to);
+    }
+  });
+  return adjacentList;
+}
+
 const bfs = function(pairs,source,target){
-  
+  const visited = [];
+  const queue = [source];
+  let found = false;
+  const adjacencyList = getAdjacentList(pairs);
+  while (queue.length > 0 && found === false){
+    let currSource = queue.shift();
+    let listToSearch = adjacencyList[currSource] || [];
+    listToSearch.forEach(element => {
+      if (element===target) {
+        found = true;
+      }
+      if (!(queue.includes(element))) {
+        queue.push(element);
+      }
+    });
+    visited.push(currSource);
+  }
+  return found;
 };
 
 module.exports = {bfs};
