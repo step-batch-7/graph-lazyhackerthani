a71bb11cd4b093dc0ac66ffd656e536b286bfdd5
thanya <32271882+ThanyaSanthosh@users.noreply.github.com>
2020-06-12 00:16:20 +0530

    bug fixed

--- src/graph.js
+++ src/graph.js
@@ -17,15 +17,15 @@ const getAdjacentList = function (pairs) {
 }
 
 const bfs = function(pairs,source,target){
-  const adjacencyList = getAdjacentList(pairs);
   const visited = [];
-  const queue = [...adjacencyList[source]];
+  const adjacencyList = getAdjacentList(pairs);
+  const queue = [source];
   let found = false;
   while (queue.length > 0 && found === false){
     let currSource = queue.shift();
     let listToSearch = adjacencyList[currSource] || [];
     listToSearch.forEach(element => {
-      if (!(queue.includes(element) || visited.includes(element))) {
+      if (!(queue.includes(element)) && !(visited.includes(element))) {
         queue.push(element);
       }
     });
